8e3326d1635f6c4cee70917fde72f04207077576
Rayane Ali <aray98@proton.me>
2025-05-10 02:01:03 +0200

    ci: add read more logic

--- script.js
+++ script.js
@@ -24,6 +24,28 @@ document.addEventListener("DOMContentLoaded", (event) => {
       },
     },
   });
+
+  /***
+   *      _____  ______          _____    __  __  ____  _____  ______   ____ _______ _   _
+   *     |  __ \|  ____|   /\   |  __ \  |  \/  |/ __ \|  __ \|  ____| |  _ \__   __| \ | |
+   *     | |__) | |__     /  \  | |  | | | \  / | |  | | |__) | |__    | |_) | | |  |  \| |
+   *     |  _  /|  __|   / /\ \ | |  | | | |\/| | |  | |  _  /|  __|   |  _ <  | |  | . ` |
+   *     | | \ \| |____ / ____ \| |__| | | |  | | |__| | | \ \| |____  | |_) | | |  | |\  |
+   *     |_|  \_\______/_/    \_\_____/  |_|  |_|\____/|_|  \_\______| |____/  |_|  |_| \_|
+   *
+   *
+   */
+  const readMoreBtn = document.querySelector(".read-more-button");
+  const readMoreBtnText = document.querySelector(".read-more");
+  const projectDetails = document.querySelector(".details-project");
+
+  readMoreBtn.addEventListener("click", () => {
+    projectDetails.classList.toggle("expanded");
+    readMoreBtnText.textContent = projectDetails.classList.contains("expanded")
+      ? "Read less"
+      : "Read more";
+  });
+
   /***
    *       _____  _____         _____
    *      / ____|/ ____|  /\   |  __ \
